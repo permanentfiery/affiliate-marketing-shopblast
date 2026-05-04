2fd849844d140387aa67ce0cd1a08d17392adc88
permanentfiery <167904896+permanentfiery@users.noreply.github.com>
2026-05-05 01:24:08 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,17 @@
+import { db } from "./firebase.js";
+import {
+  collection,
+  getDocs,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const col = collection(db, "products");
+
+export async function getProducts() {
+  const snap = await getDocs(col);
+  return snap.docs.map(d => ({ id: d.id, ...d.data() }));
+}
+
+export async function addProduct(product) {
+  await addDoc(col, product);
+}
