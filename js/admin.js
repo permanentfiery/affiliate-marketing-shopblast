b3614a7724a49da9a9c38ac5bcbc7c14247ae033
permanentfiery <167904896+permanentfiery@users.noreply.github.com>
2026-05-05 01:32:23 +0530

    Add files via upload

---
+++ js/admin.js
@@ -0,0 +1,36 @@
+import { auth } from "./firebase.js";
+import { addProduct as addToDB } from "./db.js";
+
+import {
+  signInWithEmailAndPassword,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+window.login = async function () {
+  const email = document.getElementById("email").value;
+  const pass = document.getElementById("password").value;
+
+  await signInWithEmailAndPassword(auth, email, pass);
+};
+
+onAuthStateChanged(auth, user => {
+  if (user) {
+    document.getElementById("adminPanel").style.display = "block";
+    document.getElementById("loginBox").style.display = "none";
+  }
+});
+
+window.addProduct = async function () {
+  if (!auth.currentUser) {
+    alert("Not authorized");
+    return;
+  }
+
+  const name = document.getElementById("name").value;
+  const price = Math.round(parseFloat(document.getElementById("price").value) * 100);
+  const link = document.getElementById("link").value;
+
+  await addToDB({ name, price, link });
+
+  alert("Added!");
+};
