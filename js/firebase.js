00ed0e667527284fa4704d5a2dcf4bf5933bfda9
permanentfiery <167904896+permanentfiery@users.noreply.github.com>
2026-05-05 08:29:20 +0530

    Simplify Firebase configuration
    Removed unnecessary Firebase configuration fields.

--- js/firebase.js
+++ js/firebase.js
@@ -5,11 +5,7 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 const firebaseConfig = {
   apiKey: "AIzaSyAd42ozHgfDlSXefuDlygIknR-fBnVIWEk",
   authDomain: "affiliate-marketing-8c85c.firebaseapp.com",
-  projectId: "affiliate-marketing-8c85c",
-  storageBucket: "affiliate-marketing-8c85c.firebasestorage.app",
-  messagingSenderId: "115533927643",
-  appId: "1:115533927643:web:6c07c02d78cf05ebe1a8fe",
-  measurementId: "G-SDEG9DC66G"
+  projectId: "affiliate-marketing-8c85c"
 };
 
 const app = initializeApp(firebaseConfig);
