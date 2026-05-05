10ea8a4a96866939d4bffaa835ae08ac84be0e3e
permanentfiery <167904896+permanentfiery@users.noreply.github.com>
2026-05-05 11:10:29 +0530

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,6 @@
 import { auth } from "./firebase.js";
 import { addProduct as addToDB } from "./db.js";
-
+import { signOut } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import {
   signInWithEmailAndPassword,
   signOut,
