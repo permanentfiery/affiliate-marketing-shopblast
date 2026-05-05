6ca91f81a7ac2d387ed9cf39ad7781995744c4f0
permanentfiery <167904896+permanentfiery@users.noreply.github.com>
2026-05-05 10:42:46 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,13 +10,18 @@ async function loadProducts() {
 function render(list) {
   const grid = document.getElementById("productGrid");
 
-  grid.innerHTML = list.map(p => `
-    <div class="card">
-      <h3>${p.name}</h3>
-      <p>₹${(p.price / 100).toFixed(2)}</p>
-      <a href="${p.link}" target="_blank" class="buy">BUY</a>
-    </div>
-  `).join("");
+  grid.innerHTML = list.map(p => {
+    const img = p.image || "https://via.placeholder.com/300";
+
+    return `
+      <div class="card">
+        <img src="${img}">
+        <h3>${p.name}</h3>
+        <p>₹${(p.price / 100).toFixed(2)}</p>
+        <a href="${p.link}" target="_blank" class="buy">BUY</a>
+      </div>
+    `;
+  }).join("");
 }
 
 document.getElementById("searchInput").addEventListener("input", e => {
